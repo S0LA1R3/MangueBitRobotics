ec4d3564e97718fe87fa6bcc703661f6089c8e33
Guilherme Braga <crazyboysfh3@gmail.com>
2023-09-14 13:47:45 -0300

    Update testCamera.py

--- testZone/testCamera.py
+++ testZone/testCamera.py
@@ -2,42 +2,31 @@ import cv2
 import pytesseract
 import numpy as np
 
-# Função para detectar uma cor específica na imagem
 def detect_color(frame, lower_bound, upper_bound, color_name):
-    # Converte a imagem para o espaço de cores HSV (Hue, Saturation, Value)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
-    # Define a faixa de cores que queremos detectar
     mask = cv2.inRange(hsv, lower_bound, upper_bound)
 
-    # Encontra os contornos na máscara
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Encontre o maior contorno
     max_contour = max(contours, key=cv2.contourArea, default=None)
     
     if max_contour is not None:
-        # Desenha um retângulo ao redor do maior objeto encontrado
         x, y, w, h = cv2.boundingRect(max_contour)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv2.putText(frame, color_name, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-# Inicialize a câmera (0 é o índice da câmera padrão)
 cap = cv2.VideoCapture(0)
 
 while True:
-    # Captura um quadro da câmera
     ret, frame = cap.read()
 
-    # Converta o quadro para escala de cinza
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     val, otsu = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
     erode = cv2.erode(otsu, np.ones((5, 5), np.uint8))
 
-    # Execute o OCR (Reconhecimento Óptico de Caracteres) na imagem
     text = pytesseract.image_to_string(erode, config='--psm 6')
 
-    # Verifique se as letras 'r', 'g' e 'b' estão presentes no texto reconhecido
     if 'r' in text.lower():
         cv2.putText(frame, 'r', (0, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
     
@@ -47,28 +36,22 @@ while True:
     if 'b' in text.lower():
         cv2.putText(frame, 'b', (0, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
 
-    # Detectar a cor vermelha
     lower_red = np.array([0, 100, 100])
     upper_red = np.array([10, 255, 255])
     detect_color(frame, lower_red, upper_red, "Vermelho")
 
-    # Detectar a cor verde
     lower_green = np.array([35, 100, 100])
     upper_green = np.array([85, 255, 255])
     detect_color(frame, lower_green, upper_green, "Verde")
 
-    # Detectar a cor azul
     lower_blue = np.array([90, 100, 100])
     upper_blue = np.array([140, 255, 255])
     detect_color(frame, lower_blue, upper_blue, "Azul")
 
-    # Exibir a imagem da câmera
     cv2.imshow('Camera', frame)
 
-    # Pressione 'q' para sair do loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Libere a câmera e feche a janela da câmera
 cap.release()
 cv2.destroyAllWindows()
