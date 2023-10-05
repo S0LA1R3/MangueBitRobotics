bbb036eea6315c2b8cc48568777706caeb2b5de9
Guilherme Braga <89932943+S0LA1R3@users.noreply.github.com>
2023-10-05 18:20:30 -0300

    a

---
+++ ultrassom.h
@@ -0,0 +1,17 @@
+
+#include <HCSR04.h>
+
+//Define os pinos do Arduino D8 e D9 como porta do ECHO e TRIGGER  
+#define TRIGGERF   30
+#define ECHOF      31
+
+#define TRIGGERE   32
+#define ECHOE      33
+
+#define TRIGGERD   34
+#define ECHOD      35
+
+// Inicializa o sensor usando os pinos TRIGGER and ECHO.
+UltraSonicDistanceSensor distanceSensorF(TRIGGERF, ECHOF);
+UltraSonicDistanceSensor distanceSensorE(TRIGGERE, ECHOE);
+UltraSonicDistanceSensor distanceSensorD(TRIGGERD, ECHOD);
